7e029e47dbb6fde7c0644802a50c9fa48fe8f51e
vijay bhaskar <vijaybhaskarmyv@gmail.com>
2022-05-23 16:48:07 +0530

    final commit

--- kafka_produce.py
+++ kafka_produce.py
@@ -2,5 +2,5 @@ from kafka import KafkaProducer
 import time
 producer = KafkaProducer(bootstrap_servers="pkc-l7pr2.ap-south-1.aws.confluent.cloud:9092")
 producer.send('lock', b'lock status')
-print("produced to broker")
+print("produced to broker of kafka")
 producer.flush()
